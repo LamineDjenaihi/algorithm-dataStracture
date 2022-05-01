5324247c9c733d48391fa50fdf23bfb92076c429
LamineDjenaihi <lamine.djenaihi07@yahoo.com>
2022-05-01 02:26:45 +0100

    Implemented BianrySearchTree

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
  const Stack  = require('./algo_collection/stack');
  const mySet = require('./algo_collection/sets');
  const { Queue, PriorityQueue }  = require('./algo_collection/queue&&PRQ');
+ const BST = require('./algo_collection/binarySearchTree');
 /* let newStack = new Stack();
 newStack.push(1);
 newStack.push(2);
@@ -13,7 +14,7 @@ setA.add("a");
  setB.add("b");
 setB.add("c");
 console.log(setA.subset(setB)); */
-let newQueue = new Queue();
+/* let newQueue = new Queue();
 newQueue.enqueue(3);
 newQueue.enqueue(2);
 newQueue.enqueue(1);
@@ -31,6 +32,29 @@ newPrioQueue.printCollection();
 newPrioQueue.dequeue();
 console.log(newPrioQueue.front());
 newPrioQueue.printCollection();
+ */
 
-
+const bst = new BST();
   
+  bst.add(9);
+  bst.add(4);
+  bst.add(17);
+  bst.add(3);
+  bst.add(6);
+  bst.add(22);
+  bst.add(5);
+  bst.add(7);
+  bst.add(20);
+  
+  console.log(bst.findMinHeight());
+  console.log(bst.findMaxHeight());
+  console.log(bst.isBalanced());
+  bst.add(10);
+  console.log(bst.findMinHeight());
+  console.log(bst.findMaxHeight());
+  console.log(bst.isBalanced());
+  console.log('inOrder: ' + bst.inOrder());
+  console.log('preOrder: ' + bst.preOrder());
+  console.log('postOrder: ' + bst.postOrder());
+  
+  console.log('levelOrder: ' + bst.levelOrder());  
